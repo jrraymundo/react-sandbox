e17bc5b8ea79138413c2a7c064ac73a6a7269b5c
jrraymundo <jerome_raymundo@outlook.com>
2022-01-06 12:21:53 +0800

    Update to latest CRA and adjust breaking changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
+  BrowserRouter,
+  Routes,
   Route,
 } from "react-router-dom";
 import './App.css';
@@ -12,28 +12,32 @@ import Main from './components/react-fundamentals/Main'
 import CssGrid from './components/css-grid'
 
 function App() {
-
   const headerContent = {
     title: 'Hello World',
     description: 'This is a description',
   }
 
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <div>
-              {/* Using spread operator to pass contents of object as props */}
-              <Header {...headerContent} />
+  const HomeComponent = () => {
+    return (
+      <div>
+        {/* Using spread operator to pass contents of object as props */}
+        <Header {...headerContent} />
 
-              {/* Manual passing of variables as props */}
-              <Main greeting="Hi this is a prop passed from app.js" numberGiven={3} />
-          </div>
-        </Route>
-        <Route path="/grid" component={CssGrid} />
-      </Switch>
-    </Router>
-  );
-}
+        {/* Manual passing of variables as props */}
+        <Main greeting="Hi this is a prop passed from app.js" numberGiven={3} />
+      </div>
+    )
+  }
+
+  return (
+    // Using react-router-dom v6 here. <Switch> is replaced by <Routes>, many more breaking changes found in docs
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeComponent />} />
+        <Route path="/grid" element={<CssGrid />} />
+      </Routes>
+    </BrowserRouter>
+  )
+ }
 
 export default App;
